23871f41a56ab5b699de13236fc8588edd771b40
skku-sungjin <skku7714@gmail.com>
2020-05-05 01:00:01 +0900

    [Add 5-7, 5-8]
    - SignUp and middleware
    Add: cors, morgan, signup controller in routes/user.js
    Fix: dispatch data(Frontend), signup saga, signup reducer
    Option: timezone in config.json

--- ch1/back/index.js
+++ ch1/back/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
 const db = require("./models");
 const userAPIRouter = require("./routes/user");
 const postAPIRouter = require("./routes/post");
@@ -7,6 +10,11 @@ const postsAPIRouter = require("./routes/posts");
 const app = express();
 
 db.sequelize.sync();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/api/user", userAPIRouter);
 app.use("/api/post", postAPIRouter);
--- ch1/back/routes/user.js
+++ ch1/back/routes/user.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
 
 router.get("/", (req, res) => {});
-router.post("/", (req, res) => {});
+router.post("/", async (req, res, next) => {
+  try {
+    const exUser = await db.User.findOne({
+      where: {
+        userId: req.body.userId,
+      },
+    });
+    if (exUser) {
+      return res.status(403).send("Already exist!");
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    const newUser = await db.User.create({
+      userId: req.body.userId,
+      nickname: req.body.nickname,
+      password: hashedPassword,
+    });
+    return res.status(200).json(newUser);
+  } catch (e) {
+    console.log(e);
+    return next(e);
+  }
+});
 router.get("/:id", (req, res) => {});
 router.post("/logout", (req, res) => {});
 router.post("/login", (req, res) => {});
